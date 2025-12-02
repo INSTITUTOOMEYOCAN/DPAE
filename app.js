a635677a2688a93cb82a87e15154f2ac7eb0a9a2
INSTITUTOOMEYOCAN <jperezt@omeyocaninstituto.edu.mx>
2025-12-02 14:31:45 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamburger');
+const navWrap = document.querySelector('.nav-wrap');
+const navLinks = document.querySelectorAll('.nav-link');
+
+if (hamburger && navWrap) {
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navWrap.classList.toggle('mobile-open');
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            navWrap.classList.remove('mobile-open');
+        });
+    });
+}
